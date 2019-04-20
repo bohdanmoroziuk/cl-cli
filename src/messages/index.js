46f49b5395e23a83997e034855ccb36ae33c1e21
Bogdan <bogdan7morozyuk@gmail.com>
2019-04-20 18:33:51 +0300

    Attach file with messages

---
+++ src/messages/index.js
@@ -0,0 +1,19 @@
+module.exports = [
+  {
+    type: 'input',
+    name: 'fullname',
+    message: 'Contact full name:',
+  }, {
+    type: 'input',
+    name: 'nickname',
+    message: 'Contact nickname:',
+  }, {
+    type: 'input',
+    name: 'email',
+    message: 'Contact email address:',
+  }, {
+    type: 'input',
+    name: 'phone',
+    message: 'Contact phone number:',
+  }, 
+];
